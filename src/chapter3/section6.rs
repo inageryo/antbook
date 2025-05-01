4e5975a93ae4c524188d9b719eff93c7722cfe7c
inageryo <ing114514@gmail.com>
2025-05-02 00:23:44 +0900

    intersection_of_two_prisms

--- src/chapter3/section6.rs
+++ src/chapter3/section6.rs
@@ -1,4 +1,5 @@
 pub mod beauty_contest;
 pub mod coneology;
+pub mod intersection_of_two_prisms;
 pub mod jack_straws;
 pub mod white_bird;
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-use antbook::chapter3::section6::beauty_contest::solve2;
+use antbook::chapter3::section6::intersection_of_two_prisms::solve;
 use proconio::input;
 
 fn main() {
     input! {
+        m: usize,
         n: usize,
-        points: [(isize, isize); n]
+        p1_list: [(f64, f64); m],
+        p2_list: [(f64, f64); n],
     }
-    println!("{}", solve2(n, &points));
+    println!("{}", solve(&p1_list, &p2_list));
 }
