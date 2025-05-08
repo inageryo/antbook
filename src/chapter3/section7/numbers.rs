c03ef2d6958d6d899e568bdfe8461f02eb104919
inageryo <ing114514@gmail.com>
2025-05-08 23:58:35 +0900

    fix numbers

--- src/chapter3/section7/numbers.rs
+++ src/chapter3/section7/numbers.rs
@@ -4,7 +4,7 @@ const M: isize = 10isize.pow(3);
 pub fn solve(n: usize) -> String {
     let a = vec![vec![6, -4], vec![1, 0]];
     let v = mod_mul_isize(&mod_pow_isize(&a, n, M), &[vec![3], vec![1]], M);
-    format!("{:03}", (v[1][0] * 2 - 1 + M) % M)
+    format!("{:03}", ((v[1][0] + M) * 2 - 1) % M)
 }
 
 #[cfg(test)]
@@ -18,6 +18,7 @@ mod tests {
     #[case(2, "027")]
     #[case(5, "935")]
     #[case(10, "047")]
+    #[case(8, "991")]
     #[case(100, "751")]
     #[case(1_234_567, "903")]
     #[case(1_000_000_000, "751")]
